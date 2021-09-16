4b27787fd1e53da54b3c18958cafbd15a24fd3fe
sunkickr <davidkoenit@gmail.com>
2021-09-16 15:09:44 -0400

    change name

--- dags/plugins-dag.py
+++ dags/plugins-dag.py
@@ -20,14 +20,14 @@ default_args = {
     'dagrun_timeout': timedelta(minutes=2),
 }
 
-@dag('plugin_dag',
+@dag('plugin_dag_test',
      schedule_interval='@daily',
      default_args=default_args,
      catchup=False,
      tags=['github scrape'],
      concurrency=60,
      max_active_runs=1)
-def plugin_dag():
+def plugin_dag_test():
 
     # Start the pipeline.
     t0 = DummyOperator(
@@ -94,5 +94,5 @@ def plugin_dag():
 
     send_to_airtable(transform_plugin_list(t0 >> get_plugin_list()))
 
-dag = plugin_dag()
+dag = plugin_dag_test()
         
