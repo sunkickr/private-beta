2f04efe18319d108f3d3104271cfe39b186887ea
sunkickr <davidkoenit@gmail.com>
2021-09-17 13:39:16 -0400

    add bash script

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM quay.io/astronomer/astro-runtime:2.1.1-5
+FROM quay.io/astronomer/astro-runtime:3.0.2
 ENV AIRFLOW__CORE__LOAD_EXAMPLES=False
---
+++ ci-cd.sh
@@ -0,0 +1,62 @@
+KEY_ID=$1
+KEY_SECRET=$2
+ORGANIZATION_ID=$3
+DEPLOYMENT_ID=$4
+
+# install jq
+# brew install jq
+
+# Create time stamp
+TAG=deploy-`date "+%Y-%m-%d-%HT%M-%S"`
+
+# Step 1. Use the Access token in the `astro auth login` command
+docker login images.astronomer-dev.cloud -u $KEY_ID -p $KEY_SECRET
+
+# Step 2. Request the Organization Id
+
+# Step 3. Build the image
+docker build . -t images.astronomer-dev.cloud/$ORGANIZATION_ID/$DEPLOYMENT_ID:$TAG
+
+# Step 4. Push the image
+docker push images.astronomer-dev.cloud/$ORGANIZATION_ID/$DEPLOYMENT_ID:$TAG
+
+# Step 5. Get the access token
+echo "get token"
+TOKEN=$( curl --location --request POST "https://auth.astronomer-dev.io/oauth/token" \
+        --header "content-type: application/json" \
+        --data-raw "{ 
+            \"client_id\": \"$KEY_ID\",
+            \"client_secret\": \"$KEY_SECRET\",
+            \"audience\": \"astronomer-ee\",
+            \"grant_type\": \"client_credentials\"}" | jq -r '.access_token' )
+echo $TOKEN
+# Step 6. Create the Image
+echo "get image id"
+IMAGE=$( curl --location --request POST "https://api.astronomer-dev.io/hub/v1" \
+        --header "Authorization: Bearer $TOKEN" \
+        --header "Content-Type: application/json" \
+        --data-raw "{
+            \"query\" : \"mutation imageCreate(\n    \$input: ImageCreateInput!\n) {\n    imageCreate (\n    input: \$input\n) {\n    id\n    tag\n    repository\n    digest\n    env\n    labels\n    deploymentId\n  }\n}\",
+            \"variables\" : {
+                \"input\" : {
+                    \"deploymentId\" : \"$DEPLOYMENT_ID\",
+                    \"tag\" : \"$TAG\"
+                    }
+                }
+            }" | jq -r '.data.imageCreate.id')
+echo $IMAGE
+# Step 7. Deploy the Image
+echo "deploy image"
+curl --location --request POST "https://api.astronomer-dev.io/hub/v1" \
+        --header "Authorization: Bearer $TOKEN" \
+        --header "Content-Type: application/json" \
+        --data-raw "{
+            \"query\" : \"mutation imageDeploy(\n    \$input: ImageDeployInput!\n  ) {\n    imageDeploy(\n      input: \$input\n    ) {\n      id\n      deploymentId\n      digest\n      env\n      labels\n      name\n      tag\n      repository\n    }\n}\",
+            \"variables\" : {
+                \"input\" : {
+                    \"id\" : \"$IMAGE\",
+                    \"tag\" : \"$TAG\",
+                    \"repository\" : \"images.astronomer-dev.cloud/$ORGANIZATION_ID/$DEPLOYMENT_ID\"
+                    }
+                }
+            }"
